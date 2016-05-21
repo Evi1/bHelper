24b517e6d986bd194712a8b0f11ee064ead13beb
Evi1 <w1zirn1996@gmail.com>
2016-05-21 22:46:12 +0800

    并发支持

--- main.go
+++ main.go
@@ -10,22 +10,32 @@ import (
 	"bufio"
 	//"syscall"
 	"path/filepath"
+	//"time"
 )
 
-var bPath string
-var cPath string
-var l1 *list.List
+//var bPath string
+//var cPath string
+//var l1 *list.List
+
+var routineNum chan int
+var routineLimit chan int
+
+func init() {
+	routineNum=make(chan int)
+	routineLimit=make(chan int, 4)
+}
 
 func main(){
-	bPath = "./tv.danmaku.bili/download/"
-	cPath = bPath
+	bPath := "./tv.danmaku.bili/download/"
+	cPath := bPath
 	files, _ := ioutil.ReadDir(filepath.FromSlash(cPath))
-	l1 = list.New()
+	l1 := list.New()
 	for _, f := range files {
 		if f.IsDir(){
 			l1.PushBack(f)
 		}
 	}
+	num:=0
 	for l1.Len() > 0 {
 		t := l1.Front()
 		l1.Remove(t)
@@ -34,14 +44,25 @@ func main(){
 		files, _ := ioutil.ReadDir(cPath)
 		for _, f := range files {
 			if f.IsDir(){
-				handle(f)
+				num++
+				//fmt.Println("start"+cPath+f.Name())
+				routineLimit <- 1
+				fmt.Println("routineLimit+1")
+				go handle(cPath,f)
+				//time.Sleep(time.Second)
 			}
 		}
 	}
+	//time.Sleep(1000 * time.Millisecond)
+	for i:=0;i<num;i++{
+		<-routineNum
+		fmt.Println("routineNum-1")
+	}
 }
 
-func handle(f os.FileInfo)  {
+func handle(cPath string,f os.FileInfo)  {
 	path := cPath +f.Name() + "/"
+	//fmt.Println(path)
 	files, _ := ioutil.ReadDir(filepath.FromSlash(path))
 	var title string
 	var part string
@@ -64,6 +85,10 @@ func handle(f os.FileInfo)  {
 		}
 	}
 	copyVideo(title,part,path,inPath,v)
+	<-routineLimit
+	fmt.Println("routineLimit-1")
+	routineNum<- 1
+	fmt.Println("routineNum+1")
 }
 
 func handleJSON(filename string) (string, string)  {
@@ -90,7 +115,7 @@ func copyVideo(title string, part string,path string,inPath string, v string){
 	//oldMask := syscall.Umask(0)
 	os.MkdirAll(filepath.FromSlash("./bilibili/"+title+"/"),os.ModePerm)
 	//syscall.Umask(oldMask)
-	fmt.Println(inputFile+"  ------>  "+outputFile)
+	//fmt.Println(inputFile+"  ------>  "+outputFile)
 	buf, err := ioutil.ReadFile(filepath.FromSlash(inputFile))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "File Error: %s\n", err)
