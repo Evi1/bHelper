58a9eb80ed00440e43a04a619d7601287f75713a
Evi1 <Evi1@users.noreply.github.com>
2016-05-21 15:28:54 +0800

    Update main.go

--- main.go
+++ main.go
@@ -22,7 +22,6 @@ func main(){
 	files, _ := ioutil.ReadDir(filepath.FromSlash(cPath))
 	l1 = list.New()
 	for _, f := range files {
-		//fmt.Println(f.Name())
 		if f.IsDir(){
 			l1.PushBack(f)
 		}
@@ -31,12 +30,9 @@ func main(){
 		t := l1.Front()
 		l1.Remove(t)
 		x := t.Value.(os.FileInfo)
-		//fmt.Println(x.Name())
 		cPath=bPath+x.Name()+"/"
 		files, _ := ioutil.ReadDir(cPath)
-		//fmt.Println(cPath)
 		for _, f := range files {
-			//fmt.Println(f.Name())
 			if f.IsDir(){
 				handle(f)
 			}
@@ -47,27 +43,21 @@ func main(){
 func handle(f os.FileInfo)  {
 	path := cPath +f.Name() + "/"
 	files, _ := ioutil.ReadDir(filepath.FromSlash(path))
-	//fmt.Println(path)
 	var title string
 	var part string
 	var v string
 	var inPath string
 	for _, f := range files {
-		//fmt.Println(f.Name())
 		if f.IsDir(){
 			videos,_:= ioutil.ReadDir(filepath.FromSlash(path+f.Name()+"/"))
-			//fmt.Println(path+f.Name()+"/")
 			for _, video := range videos{
-				//fmt.Println(video.Name())
 				if strings.HasSuffix(video.Name(),".mp4"){
-					//fmt.Println(video.Name())
 					v = video.Name()
 					inPath = f.Name()+"/"
 				}
 			}
 		}else if strings.HasSuffix(f.Name(),".json"){
 			title,part=handleJSON(path+f.Name())
-			//fmt.Println(title+" --- "+part)
 		}else if strings.HasSuffix(f.Name(),".mp4"){
 			v = f.Name()
 			inPath=""
@@ -100,13 +90,12 @@ func copyVideo(title string, part string,path string,inPath string, v string){
 	//oldMask := syscall.Umask(0)
 	os.MkdirAll(filepath.FromSlash("./bilibili/"+title+"/"),os.ModePerm)
 	//syscall.Umask(oldMask)
-	fmt.Println(inputFile+"------>"+outputFile)
+	fmt.Println(inputFile+"  ------>  "+outputFile)
 	buf, err := ioutil.ReadFile(filepath.FromSlash(inputFile))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "File Error: %s\n", err)
 		// panic(err.Error())
 	}
-	//fmt.Printf("%s\n", string(buf))
 	out,err:=os.OpenFile(outputFile,os.O_WRONLY|os.O_CREATE,0666)
 	if err!=nil{
 		fmt.Printf("An error occurred with file opening or creation\n")
